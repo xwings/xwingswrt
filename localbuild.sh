f6398ef9ac44befaa60078a5e68716f3d2eb71e6
xwings <kj@xandora.net>
2022-11-06 18:34:07 +0800

    v0.1

--- localbuild.sh
+++ localbuild.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-GITHUB_WORKSPACE="$(pwd)"
+COMPILE_OPTION=$1
+GITHUB_WORKSPACE="$(pwd)/AutoBuild-Actions"
+CONFIG_FILE="x86_64"
+GITHUB_ENV="${GITHUB_WORKSPACE}/AutoBuild-Action_ENV"
+CONFIG_FILE="${GITHUB_WORKSPACE}/Configs/${CONFIG_FILE}"
+DEFAULT_SOURCE="coolsnowwolf/lede:master"
+REPO_URL="https://github.com/$(cut -d \: -f 1 <<< ${DEFAULT_SOURCE})"
+REPO_BRANCH=$(cut -d \: -f 2 <<< ${DEFAULT_SOURCE})
 
 if [ ! -d AutoBuild-Actions ]; then
     git clone -b master https://github.com/xwings/AutoBuild-Actions.git AutoBuild-Actions
 fi
 
-cd ${GITHUB_WORKSPACE}/AutoBuild-Actions && git pull
+cd ${GITHUB_WORKSPACE} && git pull
+
+echo "CONFIG_FILE=$CONFIG_FILE" >> $GITHUB_ENV
+echo "Tempoary_IP=" >> $GITHUB_ENV
+echo "Tempoary_FLAG=" >> $GITHUB_ENV
+echo "REPO_URL=$REPO_URL" >> $GITHUB_ENV
+echo "REPO_BRANCH=$REPO_BRANCH" >> $GITHUB_ENV
+echo "Compile_Date=$(date +%Y%m%d%H%M)" >> $GITHUB_ENV
+echo "Display_Date=$(date +%Y/%m/%d)" >> $GITHUB_ENV
 
 cd ${GITHUB_WORKSPACE}
 if [ ! -d openwrt ]; then
@@ -17,15 +32,14 @@ cd ${GITHUB_WORKSPACE}/openwrt && git pull
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-cd ${GITHUB_WORKSPACE}/AutoBuild-Actions/
-chmod +x Scripts/AutoBuild_*.sh
-
 cd ${GITHUB_WORKSPACE}/openwrt
-source ${GITHUB_WORKSPACE}/AutoBuild-Actions/Scripts/AutoBuild_DiyScript.sh
-source ${GITHUB_WORKSPACE}/AutoBuild-Actions/Scripts/AutoBuild_Function.sh
-Firmware_Diy_Before
-rm -f .config && cp ${GITHUB_WORKSPACE}/AutoBuild-Actions/Configs/x86_64 .config
+chmod +x ${GITHUB_WORKSPACE}/Scripts/AutoBuild_*.sh
+cp ${CONFIG_FILE} ${GITHUB_WORKSPACE}/openwrt/.config
+source ${GITHUB_WORKSPACE}/Scripts/AutoBuild_DiyScript.sh
+source ${GITHUB_WORKSPACE}/Scripts/AutoBuild_Function.sh
 make defconfig
+Firmware_Diy_Before
+rm -f .config && cp ${CONFIG_FILE} ${GITHUB_WORKSPACE}/openwrt/.config
 Firmware_Diy_Main
 Firmware_Diy
 Firmware_Diy_Other
@@ -33,4 +47,3 @@ Firmware_Diy_Other
 make defconfig
 make download -j8
 make -j8
-[ "$?" == 0 ] && echo "Compile_Result=true"  || echo "Compile_Result=false"
