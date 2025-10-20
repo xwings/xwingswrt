693cf3c6344fc84126607a4df40f21677926aa16
xwings <kj@xandora.net>
2025-10-20 10:10:38 +0800

    fix airpi-ap3000e wifi module

--- customfiles/airpi-ap3000e/patch.sh
+++ customfiles/airpi-ap3000e/patch.sh
@@ -7,7 +7,7 @@ define Device/airpi-ap3000e
     DEVICE_MODEL := Airpi-AP3000E
     DEVICE_DTS := mt7981b-airpi-ap3000e
     DEVICE_DTS_DIR := ../dts
-    DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware kmod-hwmon-pwmfan kmod-usb3
+    DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware mt7986-wo-firmware mt7986-firmware kmod-hwmon-pwmfan kmod-usb3 f2fsck mkf2fs
     UBINIZE_OPTS := -E 5
     BLOCKSIZE := 128k
     PAGESIZE := 2048
@@ -16,10 +16,13 @@ define Device/airpi-ap3000e
 endef
 TARGET_DEVICES += airpi-ap3000e
     " >> lede/target/linux/mediatek/image/filogic.mk
-    sed -i '/glinet,gl-mt6000)/i \    airpi,ap3000e)\n        lan_mac=$(macaddr_generate_from_mmc_cid mmcblk0)\n        wan_mac=$(macaddr_add "$lan_mac" 1)\n        ;;' lede/target/linux/mediatek/filogic/base-files/etc/board.d/02_network
-    sed -i '/glinet,gl-mt3000/i \    airpi,ap3000e|\\' lede/target/linux/mediatek/filogic/base-files/etc/board.d/02_network
-    sed -i '/glinet,gl-mt6000|\\/i \        airpi,ap3000e|\\' lede/target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
-    sed -i '/glinet,gl-mt6000|\\/i \        airpi,ap3000e|\\\n            addr=$(macaddr_generate_from_mmc_cid mmcblk0)\n            [ "$PHYNBR" = "0" ] && macaddr_add $addr 2 > /sys${DEVPATH}/macaddress\n            [ "$PHYNBR" = "1" ] && macaddr_add $addr 3 > /sys${DEVPATH}/macaddress\n            ;;' lede/target/linux/mediatek/filogic/base-files/etc/hotplug.d/ieee80211/11_fix_wifi_mac
+    sed -i '/glinet,gl-mt6000)/i \\tairpi,ap3000e)\n\t\tlan_mac=$(macaddr_generate_from_mmc_cid mmcblk0)\n\t\twan_mac=$(macaddr_add "$lan_mac" 1)\n\t\t;;' lede/target/linux/mediatek/filogic/base-files/etc/board.d/02_network
+    sed -i '/glinet,gl-mt3000/i \\tairpi,ap3000e|\\' lede/target/linux/mediatek/filogic/base-files/etc/board.d/02_network
+    sed -i '/glinet,gl-mt6000|\\/i \\tairpi,ap3000e|\\' lede/target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+    sed -i '/glinet,gl-mt6000|\\/i \\tairpi,ap3000e)\n\t\taddr=$(macaddr_generate_from_mmc_cid mmcblk0)\n\t\t[ "$PHYNBR" = "0" ] && macaddr_add $addr 2 > /sys${DEVPATH}/macaddress\n\t\t[ "$PHYNBR" = "1" ] && macaddr_add $addr 3 > /sys${DEVPATH}/macaddress\n\t\t;;' lede/target/linux/mediatek/filogic/base-files/etc/hotplug.d/ieee80211/11_fix_wifi_mac
+    sed -i '/mt7986-wo-firmware\/install/a \\tcp $(PKG_BUILD_DIR)\/mediatek/mt7986_eeprom_mt7976_dbdc.bin $(PKG_BUILD_DIR)\/mediatek\/mt7981_eeprom_mt7976_dbdc.bin' lede/package/firmware/linux-firmware/mediatek.mk
+    sed -i '/mt7986_wo_1.bin/a \\t\t$(PKG_BUILD_DIR)/mediatek/mt7986_eeprom_mt7976_dbdc.bin \\' lede/package/firmware/linux-firmware/mediatek.mk
+    sed -i '/mt7986_wo_1.bin/a \\t\t$(PKG_BUILD_DIR)/mediatek/mt7981_eeprom_mt7976_dbdc.bin \\' lede/package/firmware/linux-firmware/mediatek.mk
 }
 
 sed -i '/pattern/a bananapi,bpi-r3|\' inputfile
